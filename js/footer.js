195def4b3090147bb83ce1362b17f11f6b1d5725
freestyle09 <jozefrzadkosz09@gmail.com>
2020-09-21 15:56:56 +0200

    finished product page

---
+++ js/footer.js
@@ -0,0 +1,4 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const date = document.getElementById("date");
+  date.textContent = new Date().getFullYear();
+});
--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
-  $("#carousel").slick({
-    arrows: false,
-    infinite: true,
-    dots: true,
-    speed: 600,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    cssEase: "ease-in-out",
-    dotsClass: "carousel__indicators indicators"
+  $("#pills-tab a").on("click", function(e) {
+    e.preventDefault();
+    $(this).tab("show");
   });
-
-  const date = document.getElementById("date");
-  date.textContent = new Date().getFullYear();
 });
